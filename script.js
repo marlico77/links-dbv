6d4c253d497ad91df32fc894021e54fb68d486d0
marlico77 <marlonssoficial@gmail.com>
2025-02-18 22:40:29 -0300

    atualização da caixa de dialogo

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-function emBreve() {
-    alert('Em Breve!')
+function mostrarDialogo() {
+    Swal.fire({
+        title: 'ATENÇÃO',
+        text: 'Em breve disponível!',
+        icon: 'error', // Pode ser 'success', 'error', 'warning', 'info', 'question'
+        confirmButtonText: 'OK',
+        customClass: {
+            popup: 'minha-caixa-dialogo',
+            title: 'meu-titulo',
+            content: 'meu-conteudo',
+            confirmButton: 'meu-botao'
+        }
+    });
 }
